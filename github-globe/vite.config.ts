e4c55b9ac2530e683e642c6152d8a5e4264d01c1
vaishnavmore2004 <morevaishnav70@gmail.com>
2026-02-26 21:32:20 +0530

    Deploy tria 2l

--- github-globe/vite.config.ts
+++ github-globe/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base : process.env.VITE_BASE_PATH || "/GeoShield-AMD-Slingshot",
+  //base : process.env.VITE_BASE_PATH || "/GeoShield-AMD-Slingshot",
   resolve: {
     alias: {
       "@": "/src",
